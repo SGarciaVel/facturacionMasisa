b14af35841c60097ef194b3ebac42853b677dc6e
SGarciaVel <sebastian.garcia1601@alumnos.ubiobio.cl>
2024-10-08 00:32:58 -0300

    Creacion de base de datos temporal con db.js actualizado

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: '',
+    user: 'postgres',
     host: 'localhost',
-    database: '',
-    password: '',
+    database: 'taller_bdd',
+    password: 'sgarcia1197',
     port: 5432,
 });
 
